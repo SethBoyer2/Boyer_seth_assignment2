464eaee7ebfbada132eaa0723493e6c94f1f5083
Seth <sboyer2@academic.rrc.ca>
2025-01-29 15:20:03 -0600

    Second Commit: Added some variables and data to seth_b_assignment_2.py

---
+++ seth_b_assignment_2.py
@@ -0,0 +1,10 @@
+# SIMPLE DATA TYPES
+name = "Seth"
+has_license = False
+current_year = 2025
+
+print("name:", name, "type:", type(name))
+print("has license:", has_license, "type:", type(has_license))
+print("this year:", current_year, "type:", type(current_year))
+current_year = current_year + 1
+print("Next year:", current_year, "type:", type(current_year))
