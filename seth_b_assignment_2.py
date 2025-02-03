93fa8a8459bf9704000c35bece75dddfdfc3c28b
Seth <sboyer2@academic.rrc.ca>
2025-02-02 22:45:50 -0600

    Fourth Commit: Added new lists and made changes to said lists.

--- seth_b_assignment_2.py
+++ seth_b_assignment_2.py
@@ -29,3 +29,38 @@ print("Purchase price:", car_price, "Provincial tax:",
        pst_total, "Federal tax", gst_total, "Total", final_price)
 
 print(f"Purchase price: ${car_price:,.2f} Provincial Tax: ${pst_total:,.2f} Federal Tax: ${gst_total:,.2f} Total: ${final_price:,.2f}")
+
+
+#LISTS
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+my_second_list = ["A", "B", "C"]
+my_newest_list = []
+
+print(type(my_list))
+my_list.insert(5, "Seth")
+print(my_list)
+my_list.remove(9)
+print(my_list)
+
+my_newest_list.insert(0, my_list[0])
+my_newest_list.insert(1, my_list[1])
+my_newest_list.insert(2, my_list[2])
+my_newest_list.insert(3, my_list[3])
+my_newest_list.insert(4, my_list[4])
+my_newest_list.insert(5, my_list[5])
+my_newest_list.insert(6, my_list[6])
+my_newest_list.insert(7, my_list[7])
+my_newest_list.insert(8, my_list[8])
+my_newest_list.insert(9, my_list[9])
+my_newest_list.insert(10, my_second_list[0])
+my_newest_list.insert(11, my_second_list[1])
+my_newest_list.insert(12, my_second_list[2])
+print(my_newest_list)
+
+
+
+
+
+
+
+
